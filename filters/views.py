3e5863406e9c5516fc4b93a87af5214dce149461
Amine <mohamedremmas1@gmail.com>
2025-10-03 10:52:49 -0300

    add API views for Filter and SensorData

--- filters/views.py
+++ filters/views.py
@@ -1,6 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .models import Filter, SensorData
+from .serializers import FilterSerializer, SensorDataSerializer
 
-def index(request):
-    return HttpResponse("Bienvenue sur la gestion des filtres !")
+# Liste et création des filtres
+class FilterListCreateView(generics.ListCreateAPIView):
+    queryset = Filter.objects.all()
+    serializer_class = FilterSerializer
 
+
+# Détails d’un filtre spécifique (lecture, mise à jour, suppression)
+class FilterDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Filter.objects.all()
+    serializer_class = FilterSerializer
+
+
+# Liste et création des données capteurs
+class SensorDataListCreateView(generics.ListCreateAPIView):
+    queryset = SensorData.objects.all()
+    serializer_class = SensorDataSerializer
+
+
+# Détails d’une donnée capteur spécifique
+class SensorDataDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = SensorData.objects.all()
+    serializer_class = SensorDataSerializer
